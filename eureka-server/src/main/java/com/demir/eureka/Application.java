edebbc2d2cd237b9b920f442e329c03f87647db9
muratdemir <murrat.demirr@gmail.com>
2018-02-03 23:19:17 +0300

    eureka served added

---
+++ eureka-server/src/main/java/com/demir/eureka/Application.java
@@ -0,0 +1,20 @@
+package com.demir.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * User: muratdemir
+ * Date: 3.02.2018
+ * Time: 23:14
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
